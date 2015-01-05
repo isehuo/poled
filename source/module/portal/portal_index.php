f6230b6272e4ffc1a1c8f242c24f03ae4a162a12
isehuo <daqiu.fe@gmail.com>
2015-01-05 22:14:30 +0800

    add focus

--- source/module/portal/portal_index.php
+++ source/module/portal/portal_index.php
@@ -29,5 +29,39 @@ if(isset($_G['makehtml'])){
 	helper_makehtml::portal_index();
 }
 
+$fe_focuslist = array();
+$fe_focuslist[] = array(
+    'title' => "成教量身定做",
+    'desc' => '<p>为早教、幼教、成教量身定做<br />与行业团体、领袖精诚合作，保障我们的系统符合行业现在与未来<br />基于互联网思维管理教育机构</p>',
+    'pic' => 'http://a.hiphotos.baidu.com/image/w%3D2048%3Bq%3D90/sign=d1d88b19df54564ee565e33987e6a7f3/29381f30e924b899318dce326c061d950a7bf603.jpg',
+    'pic_thum' => '',
+    'bgcolor' => '#05b8e0'
+);
+
+$fe_focuslist[] = array(
+    'title' => "成教量身定做",
+    'desc' => '<p>为早教、幼教、成教量身定做<br />与行业团体、领袖精诚合作，保障我们的系统符合行业现在与未来<br />基于互联网思维管理教育机构</p>',
+    'pic' => 'http://a.hiphotos.baidu.com/image/w%3D2048%3Bq%3D90/sign=d1d88b19df54564ee565e33987e6a7f3/29381f30e924b899318dce326c061d950a7bf603.jpg',
+    'pic_thum' => '',
+    'bgcolor' => '#ff6600'
+);
+
+$fe_focuslist[] = array(
+    'title' => "成教量身定做",
+    'desc' => '<p>为早教、幼教、成教量身定做<br />与行业团体、领袖精诚合作，保障我们的系统符合行业现在与未来<br />基于互联网思维管理教育机构</p>',
+    'pic' => 'http://a.hiphotos.baidu.com/image/w%3D2048%3Bq%3D90/sign=d1d88b19df54564ee565e33987e6a7f3/29381f30e924b899318dce326c061d950a7bf603.jpg',
+    'pic_thum' => '',
+    'bgcolor' => '#0288ce'
+);
+
+$fe_focuslist[] = array(
+    'title' => "成教量身定做",
+    'desc' => '<p>为早教、幼教、成教量身定做<br />与行业团体、领袖精诚合作，保障我们的系统符合行业现在与未来<br />基于互联网思维管理教育机构</p>',
+    'pic' => 'http://a.hiphotos.baidu.com/image/w%3D2048%3Bq%3D90/sign=d1d88b19df54564ee565e33987e6a7f3/29381f30e924b899318dce326c061d950a7bf603.jpg',
+    'pic_thum' => '',
+    'bgcolor' => '#669900'
+);
+
+
 include_once template('diy:portal/index');
 ?>
